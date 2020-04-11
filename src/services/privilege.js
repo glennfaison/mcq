2b6696f0f4d6371a56476c3993cff38fb5c5071f
glennfaison <glennfaison@gmail.com>
2020-04-11 04:54:51 +0100

    Create  and  REST endpoints

--- src/services/privilege.js
+++ src/services/privilege.js
@@ -1,11 +1,19 @@
-const Privilege = require('../models/privilege');
+const PrivilegeModel = require('../models/privilege');
 const mixin = require('../../helpers/mixin');
 
-class _PrivilegeService {}
+class _PrivilegeService {
+  findOneAndUpdate (conditions, properties) {
+    return PrivilegeModel.findOneAndUpdate(conditions, properties, { new: true });
+  }
+
+  findByIdAndUpdate (id, properties) {
+    return this.findOneAndUpdate({ _id: id }, properties);
+  }
+}
 
 /** @typedef {_PrivilegeService & import('mongoose').Model} PrivilegeService */
 
 /** @type {PrivilegeService} */
-const service = mixin(Privilege, new _PrivilegeService());
+const service = mixin(PrivilegeModel, new _PrivilegeService());
 
 module.exports = service;
--- src/services/quiz.js
+++ src/services/quiz.js
@@ -1,11 +1,19 @@
-const Quiz = require('../models/quiz');
+const QuizModel = require('../models/quiz');
 const mixin = require('../../helpers/mixin');
 
-class _QuizService {}
+class _QuizService {
+  findOneAndUpdate (conditions, properties) {
+    return QuizModel.findOneAndUpdate(conditions, properties, { new: true });
+  }
+
+  findByIdAndUpdate (id, properties) {
+    return this.findOneAndUpdate({ _id: id }, properties);
+  }
+}
 
 /** @typedef {_QuizService & import('mongoose').Model} QuizService */
 
 /** @type {QuizService} */
-const service = mixin(Quiz, new _QuizService());
+const service = mixin(QuizModel, new _QuizService());
 
 module.exports = service;
