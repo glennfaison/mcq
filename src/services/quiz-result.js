ae4c3e726205bea90f9443d9d263ed96b21b4f88
glennfaison <glennfaison@gmail.com>
2020-04-11 05:04:34 +0100

    Create  REST endpoint

--- src/services/quiz-result.js
+++ src/services/quiz-result.js
@@ -1,11 +1,19 @@
-const QuizResult = require('../models/quiz-result');
+const QuizResultModel = require('../models/quiz-result');
 const mixin = require('../../helpers/mixin');
 
-class _QuizResultService {}
+class _QuizResultService {
+  findOneAndUpdate (conditions, properties) {
+    return QuizResultModel.findOneAndUpdate(conditions, properties, { new: true });
+  }
+
+  findByIdAndUpdate (id, properties) {
+    return this.findOneAndUpdate({ _id: id }, properties);
+  }
+}
 
 /** @typedef {_QuizResultService & import('mongoose').Model} QuizResultService */
 
 /** @type {QuizResultService} */
-const service = mixin(QuizResult, new _QuizResultService());
+const service = mixin(QuizResultModel, new _QuizResultService());
 
 module.exports = service;
