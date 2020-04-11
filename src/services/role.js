325061c20dce398523ab90aab6e9424f1b08b82f
glennfaison <glennfaison@gmail.com>
2020-04-11 04:45:25 +0100

    Create  REST endpoint

--- src/services/role.js
+++ src/services/role.js
@@ -1,7 +1,15 @@
 const Role = require('../models/role');
 const mixin = require('../../helpers/mixin');
 
-class _RoleService {}
+class _RoleService {
+  findOneAndUpdate (conditions, properties) {
+    return Role.findOneAndUpdate(conditions, properties, { new: true });
+  }
+
+  findByIdAndUpdate (id, properties) {
+    return this.findOneAndUpdate({ _id: id }, properties);
+  }
+}
 
 /** @typedef {_RoleService & import('mongoose').Model} RoleService */
 
