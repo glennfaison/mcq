cc29bf190d6516482efc9f2f6dcd7c703903924d
glennfaison <glennfaison@gmail.com>
2020-04-13 02:35:59 +0100

    Remove circular dependency in helpers/index.js

--- api/helpers/index.js
+++ api/helpers/index.js
@@ -2,6 +2,5 @@ module.exports = {
   initializeTest: require('./initialize-test'),
   mixin: require('./mixin'),
   mongodbHelper: require('./mongodb'),
-  userHelper: require('./user'),
-  GenericCrudService: require('./generic-crud-service')
+  userHelper: require('./user')
 };
